a3dc6ae60acc34561c077319e78ae452bfa41044
GolamMuid <93812468+GolamMuid@users.noreply.github.com>
2021-11-12 19:19:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,116 @@
+//> All variable Declaration
+
 let input = document.querySelector("#input");
 let output = document.querySelector("#output");
 
-document.addEventListener("keydown", text);
-// document.addEventListener("keyup", text);
+let buttonBold = document.querySelector("#bold");
+let buttonItalic = document.querySelector("#italic");
+let buttonUnderlined = document.querySelector("#underlined");
 
-function text() {
-  output.value = input.value;
-}
-
-function bold() {
-  if (output.style.fontWeight == "bold") output.style.fontWeight = "normal";
-  else output.style.fontWeight = "bold";
-}
-
-function italic() {
-  if (output.style.fontStyle == "italic") output.style.fontStyle = "normal";
-  else output.style.fontStyle = "italic";
-}
-
-function underlined() {
-  if (output.style.textDecoration == "underline")
-    output.style.textDecoration = "none";
-  else output.style.textDecoration = "underline";
-}
-
-function left() {
-  output.style.textAlign = "left";
-}
-function center() {
-  output.style.textAlign = "center";
-}
-function right() {
-  output.style.textAlign = "right";
-}
-
-// let fsize = window.getComputedStyle(output, null).getPropertyValue("font-size");
-// fsize = parseFloat(fsize);
-
-// function sizechange(change) {
-//   if (change === "increase") {
-//     output.style.fontSize = fsize + 6 + "px";
-//   } else {
-//     output.style.fontSize = fsize - 6 + "px";
-//   }
-// }
+let leftAlign = document.querySelector("#leftAlign");
+let centerAlign = document.querySelector("#centerAlign");
+let rightAlign = document.querySelector("#rightAlign");
 
 let increase = document.querySelector("#increase");
 let decrease = document.querySelector("#decrease");
 
 let fsize = window.getComputedStyle(output, null).getPropertyValue("font-size");
 fsize = parseFloat(fsize);
+
+// Variables end here<
+
+//> Function to copy texts
+
+// document.addEventListener("keydown", text);
+document.addEventListener("keyup", text);
+
+function text() {
+  console.log(input.value);
+  output.value = input.value;
+}
+
+// Function ends here<
+
+//> Bold Italic and Underlined Buttons
+
+function bold() {
+  if (output.style.fontWeight == "bold") {
+    output.style.fontWeight = "normal";
+  } else {
+    output.style.fontWeight = "bold";
+  }
+
+  if (buttonBold.classList.contains("active")) {
+    buttonBold.classList.remove("active");
+  } else {
+    buttonBold.classList.add("active");
+  }
+}
+
+function italic() {
+  if (output.style.fontStyle == "italic") {
+    output.style.fontStyle = "normal";
+  } else {
+    output.style.fontStyle = "italic";
+  }
+
+  if (buttonItalic.classList.contains("active")) {
+    buttonItalic.classList.remove("active");
+  } else {
+    buttonItalic.classList.add("active");
+  }
+}
+
+function underlined() {
+  if (output.style.textDecoration == "underline") {
+    output.style.textDecoration = "none";
+  } else {
+    output.style.textDecoration = "underline";
+  }
+  if (buttonUnderlined.classList.contains("active")) {
+    buttonUnderlined.classList.remove("active");
+  } else {
+    buttonUnderlined.classList.add("active");
+  }
+}
+
+//Bold Italic and Underlined buttons end here<
+
+//>Alignment Buttons
+
+function left() {
+  output.style.textAlign = "left";
+
+  // if (output.style.textAlign == "left") {
+  //   leftAlign.classList.add("active");
+  // } else {
+  //   leftAlign.classList.remove("active");
+  // }
+}
+
+function center() {
+  output.style.textAlign = "center";
+
+  // if (output.style.textAlign == "center") {
+  //   centerAlign.classList.add("active");
+  // } else {
+  //   centerAlign.classList.remove("active");
+  // }
+}
+
+function right() {
+  output.style.textAlign = "right";
+
+  // if (output.style.textAlign == "right") {
+  //   rightAlign.classList.add("active");
+  // } else {
+  //   rightAlign.classList.remove("active");
+  // }
+}
+
+//<Alignment Buttons end here
+
+//> Font size increase-decrease
 
 increase.addEventListener("click", function () {
   fsize = fsize + 6;
@@ -60,3 +121,18 @@ decrease.addEventListener("click", function () {
   fsize = fsize - 6;
   output.style.fontSize = fsize + "px";
 });
+
+// Font size increase-decrease ends here<
+
+// > Font size manual increas-decrease
+
+document.addEventListener("keyup", fontfix);
+
+function fontfix() {
+  let fontNumber = document.querySelector("#fontNumber").value;
+  fontNumber = parseInt(fontNumber);
+  console.log(fontNumber);
+  output.style.fontSize = fontNumber + "px";
+}
+
+// Font size manual increas-decrease <
